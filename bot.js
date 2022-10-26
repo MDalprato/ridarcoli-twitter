f62d944867e97b04b21aa212cef7d3101e83ae89
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2022-10-26 22:40:10 +0200

    Fix twitt issue

--- bot.js
+++ bot.js
@@ -1,16 +1,47 @@
 require('dotenv').config();
 var Twit = require('twit');
+const request = require('request');
 
 var T = new Twit({
-  consumer_key:  process.env.CONSUMER_KEY,  
-  consumer_secret: process.env.CONSUMER_SECRET ,
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-//
-//  tweet 'hello world!'
-//
-T.post('statuses/update', { status: 'test1' }, function(err, data, response) {
-  console.log(data);
-})
+
+const getDataFromRidracoli = async () => {
+
+  const url = "https://www.romagnacque.it/datidiretta/index2.php";
+  let options = { json: true };
+
+  request(url, options, (error, res, body) => {
+    if (error) {
+      return console.log(error)
+    };
+
+    if (!error && res.statusCode == 200) {
+
+      const ridracoliObj = body;
+
+      const maxVolumeLevel = 33060000;
+      const currentVolumeLevel = ridracoliObj.volumeInvaso;
+      const volumePercentage = Math.round(currentVolumeLevel * 100 / maxVolumeLevel).toFixed(2);
+
+      const airTemperature = ridracoliObj.idrometeoRidraccoli.CtemperaturaAria;
+
+
+      const textToTwitt = `Il volume dell'invaso è al ${volumePercentage} %, la temperatura è di ${airTemperature} °`
+
+      console.log(textToTwitt);
+
+      // T.post('statuses/update', { status: textToTwitt }, function (err, data, response) {
+      //   console.log(data);
+      // })
+
+    };
+  });
+
+};
+
+getDataFromRidracoli();
